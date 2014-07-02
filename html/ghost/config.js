c70a8fc67375801041ad890bd75775d2f5938cd2
Aaron Saarela <aaron.saarela@gmail.com>
2014-07-02 20:12:43 +0000

    Initial layout.

---
+++ html/ghost/config.js
@@ -0,0 +1,32 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+// Documentation can be found at http://docs.ghost.org/usage/configuration/
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: 'http://www.aaronsaarela.net/blog',
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: true
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        }
+    }
+};
+
+// Export config
+module.exports = config;
